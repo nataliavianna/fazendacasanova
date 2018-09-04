fe7e9f882f71e8a4230a297f02b8972446440fee
Natalia Vianna <natalia.viannao@gmail.com>
2018-09-04 16:01:47 -0300

    chiltheme

---
+++ wp-content/themes/farmtheme/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Twenty Seventeen functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ */
+
+/**
+ * Twenty Seventeen only works in WordPress 4.7 or later.
+ */
